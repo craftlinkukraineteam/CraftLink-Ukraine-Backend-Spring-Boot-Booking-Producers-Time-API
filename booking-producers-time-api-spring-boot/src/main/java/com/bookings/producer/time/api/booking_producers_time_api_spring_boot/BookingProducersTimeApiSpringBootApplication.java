670fdeb677ffd25ce0c6a8ade3fe82753ce20b8e
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-12-17 02:13:52 +0200

    second commit

---
+++ booking-producers-time-api-spring-boot/src/main/java/com/bookings/producer/time/api/booking_producers_time_api_spring_boot/BookingProducersTimeApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bookings.producer.time.api.booking_producers_time_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingProducersTimeApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingProducersTimeApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ booking-producers-time-api-spring-boot/src/test/java/com/bookings/producer/time/api/booking_producers_time_api_spring_boot/BookingProducersTimeApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookings.producer.time.api.booking_producers_time_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingProducersTimeApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
